d536e83822809c79d4a0dbff831f9b8a0329f885
Francisco Castillo <frank.gamedev@gmail.com>
2026-01-12 13:10:14 -0500

    will check 1000 possible ToolMenus and return their names on a list.

--- ue5_toolmenus_checker.py
+++ ue5_toolmenus_checker.py
@@ -1,6 +1,18 @@
 import unreal
 
+def list_menu(num=1000):
+    menu_list = set()
+    for i in range (num):
+        obj = unreal.find_object(None, f"/Engine/Transient.ToolMenus_0:RegisteredMenu{i}")    
+        if not obj:
+            continue
 
+        menu_name = str(obj.menu_name)
 
-def list_menu():
-    pass
+        if menu_name == "None":
+            continue
+        
+        menu_list.add(menu_name)
+    return list(menu_list)
+
+print(list_menu())
