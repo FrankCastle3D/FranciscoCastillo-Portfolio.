4046652f6a92f639129e01b28f65ae1818715f6d
Francisco Castillo <66626901+FrankCastle3D@users.noreply.github.com>
2026-01-12 13:39:26 -0500

    Add docstring and newline to ue5_toolmenus_checker.py
    Added a description with instructions on the top portion of the file

--- ue5_toolmenus_checker.py
+++ ue5_toolmenus_checker.py
@@ -1,3 +1,12 @@
+"""
+This tools allows you to find the names of all the windows and buttons on the UE5 interface.
+Then you could define a menu as follows -> menus = unreal.ToolMenus.get()
+And then assign a specific menu to a variable -> main_menu = menus.find_menu("LevelEditor.MainMenu")
+
+Then you could expand such menu with the following command:
+custom_menu = main_menu.add_sub_menu("Custom Menu", "Custom Menu Entry", "Menu Name", "Menu Label")
+
+"""
 import unreal
 
 def list_menu(num=1000):
@@ -16,3 +25,4 @@ def list_menu(num=1000):
     return list(menu_list)
 
 print(list_menu())
+
