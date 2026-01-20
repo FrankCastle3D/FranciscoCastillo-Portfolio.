4f62191cfc2ed8a440231836fdf5138010b17c88
Francisco Castillo <66626901+FrankCastle3D@users.noreply.github.com>
2026-01-19 19:36:15 -0500

    Create Functions struct with addNumbers method
    Added a struct with a function to add two numbers.

---
+++ CustomNode_Structs.hlsl
@@ -0,0 +1,12 @@
+// On UE5 You can add functions through a struct.
+
+// Example
+struct Functions(){
+  float addNumbers(float a, float b){
+    return a + b;
+  }
+}
+
+Functions = f;
+
+f.addNumbers(1,2);
